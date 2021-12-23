cf3dd0ffffe0b7f8a93e7485b6328a89bc263e35
Sascha Moecker <Sascha.Moecker@bmw.de>
2021-12-23 12:45:20 +0100

    Add GPX

---
+++ gpx.py
@@ -0,0 +1,37 @@
+import gpxpy
+import gpxpy.gpx
+
+# Parsing an existing file:
+# -------------------------
+
+gpx_file = open("data/example.gpx", "r")
+
+gpx = gpxpy.parse(gpx_file)
+
+for track in gpx.tracks:
+    for segment in track.segments:
+        for point in segment.points:
+            print("Point at ({0},{1}) -> {2}".format(point.latitude, point.longitude, point.elevation))
+
+for waypoint in gpx.waypoints:
+    print("waypoint {0} -> ({1},{2})".format(waypoint.name, waypoint.latitude, waypoint.longitude))
+
+for route in gpx.routes:
+    print("Route:")
+    for point in route.points:
+        print("Point at ({0},{1}) -> {2}".format(point.latitude, point.longitude, point.elevation))
+
+print("GPX:", gpx.to_xml())
+gpx = gpxpy.gpx.GPX()
+
+gpx_track = gpxpy.gpx.GPXTrack()
+gpx.tracks.append(gpx_track)
+
+gpx_segment = gpxpy.gpx.GPXTrackSegment()
+gpx_track.segments.append(gpx_segment)
+
+gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1234, 5.1234, elevation=1234))
+gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1235, 5.1235, elevation=1235))
+gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1236, 5.1236, elevation=1236))
+
+print("Created GPX:", gpx.to_xml())
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import gpxpy
-import gpxpy.gpx
+import distance as distance
 
-# Parsing an existing file:
-# -------------------------
+munich = (48.13743, 11.57549)
+gps = gpxpy.gpx.GPXTrackPoint(munich[0], munich[1])
+
+print(distance.haversine(munich, (gps.latitude, gps.longitude)))
 
 gpx_file = open("data/example.gpx", "r")
 
@@ -11,27 +13,4 @@ gpx = gpxpy.parse(gpx_file)
 for track in gpx.tracks:
     for segment in track.segments:
         for point in segment.points:
-            print("Point at ({0},{1}) -> {2}".format(point.latitude, point.longitude, point.elevation))
-
-for waypoint in gpx.waypoints:
-    print("waypoint {0} -> ({1},{2})".format(waypoint.name, waypoint.latitude, waypoint.longitude))
-
-for route in gpx.routes:
-    print("Route:")
-    for point in route.points:
-        print("Point at ({0},{1}) -> {2}".format(point.latitude, point.longitude, point.elevation))
-
-print("GPX:", gpx.to_xml())
-gpx = gpxpy.gpx.GPX()
-
-gpx_track = gpxpy.gpx.GPXTrack()
-gpx.tracks.append(gpx_track)
-
-gpx_segment = gpxpy.gpx.GPXTrackSegment()
-gpx_track.segments.append(gpx_segment)
-
-gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1234, 5.1234, elevation=1234))
-gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1235, 5.1235, elevation=1235))
-gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1236, 5.1236, elevation=1236))
-
-print("Created GPX:", gpx.to_xml())
+            print(point)
